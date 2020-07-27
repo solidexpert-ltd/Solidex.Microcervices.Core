085093ebd5d37ce8961915baddef90447a032e97
Юрик Хмелюк <yurick.hmeluk@gmail.com>
2020-07-27 16:09:35 +0300

    add signalr

---
+++ Infrasructure/SignalR/BroadcastMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Microcervices.Core.Infrasructure.SignalR
+{
+    public class BroadcastMessage
+    {
+        /// <summary>
+        /// Type of Message
+        /// </summary>
+        public string MessageType { get; set; }
+
+        /// <summary>
+        /// Type of Data
+        /// </summary>
+        public object Data { get; set; }
+    }
+}
