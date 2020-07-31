a60ea8d93e9ffd4ac5fd77ff98e38d7b0329f14b
Юрик Хмелюк <yurick.hmeluk@gmail.com>
2020-07-31 12:05:30 +0300

    add jwt client

---
+++ JwtAuth/JwtHttpClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Solidex.Core.Base.ComplexTypes;
+
+namespace Microcervices.Core.JwtAuth
+{
+    public class JwtHttpClient: HttpClient
+    {
+        public JwtHttpClient(string hostUrl, IHttpContextAccessor accessor): base(new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+        })
+        {
+            Timeout = TimeSpan.FromMinutes(1);
+            DefaultRequestHeaders.Add("Accept", "application/json");
+
+            if (accessor.HttpContext.Request.Headers.ContainsKey("Authorization"))
+            {
+                DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessor.HttpContext.Request.Headers["Authorization"]);
+            }
+            else
+            {
+                DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", JwtTokenProvider.GenerateSystemToken(SystemUsersEnumeration.SystemMessage));
+            }
+
+            BaseAddress = new Uri(hostUrl);
+        }
+        
+    }
+}
