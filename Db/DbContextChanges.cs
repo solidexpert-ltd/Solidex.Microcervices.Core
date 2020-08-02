2be855540426eb177301883234038d808b499512
siarhei shchypanau <breate.by@gmail.com>
2020-08-03 02:19:11 +0300

    all

--- Db/DbContextChanges.cs
+++ Db/DbContextChanges.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Solidex.Core.Data.Infrastructure;
+using Solidex.Core.Base.Abstraction;
 
 namespace Microcervices.Core.Db
 {
