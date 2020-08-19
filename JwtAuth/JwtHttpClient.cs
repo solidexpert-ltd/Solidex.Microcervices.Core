2407b65a91d15f19a73e9cc933b3b13a5c15aaef
Юрик Хмелюк <yurick.hmeluk@gmail.com>
2020-08-19 11:37:13 +0300

    add rabbitmq

--- JwtAuth/JwtHttpClient.cs
+++ JwtAuth/JwtHttpClient.cs
@@ -10,6 +10,17 @@ namespace Microcervices.Core.JwtAuth
 {
     public class JwtHttpClient: HttpClient
     {
+        public JwtHttpClient(string hostUrl): base(new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+        })
+        {
+            Timeout = TimeSpan.FromMinutes(1);
+            DefaultRequestHeaders.Add("Accept", "application/json");
+            DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", JwtTokenProvider.GenerateSystemToken(SystemUsersEnumeration.SystemMessage));
+
+            BaseAddress = new Uri(hostUrl);
+        }
         public JwtHttpClient(string hostUrl, IHttpContextAccessor accessor): base(new HttpClientHandler
         {
             ServerCertificateCustomValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
