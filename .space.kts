41fadd008d0a296e92797e711da69748b9990478
Siarhei Shchypanau <siarhei.shchypanau@gmail.com>
2022-06-15 12:19:09 +0000

    update feed

--- .space.kts
+++ .space.kts
@@ -1,6 +1,6 @@
 job(".NET Core desktop. Build, test, publish"){
     container(image = "mcr.microsoft.com/dotnet/core/sdk:3.1"){
-        env["FEED_URL"] = "https://nuget.pkg.jetbrains.space/mycompany/p/pkey/mynuget/v3/index.json"
+        env["FEED_URL"] = "https://nuget.pkg.jetbrains.space/solidexpert/p/scrm/nuget/v3/index.json"
         shellScript {
             content = """
                 echo Run build...
