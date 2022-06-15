62c76951d7942bad3cc8b704562838a861963aec
Siarhei Shchypanau <siarhei.shchypanau@gmail.com>
2022-06-15 12:17:01 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,16 @@
+job(".NET Core desktop. Build, test, publish"){
+    container(image = "mcr.microsoft.com/dotnet/core/sdk:3.1"){
+        env["FEED_URL"] = "https://nuget.pkg.jetbrains.space/mycompany/p/pkey/mynuget/v3/index.json"
+        shellScript {
+            content = """
+                echo Run build...
+                dotnet build
+                echo Run tests...
+                dotnet test ./MainTests/
+                echo Publish NuGet package...
+                chmod +x publish.sh
+                ./publish.sh
+            """
+        }
+    }
+}
