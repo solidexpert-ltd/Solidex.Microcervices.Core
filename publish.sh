ec1bc3fe3ca6938cca127956a597be773f10d001
Siarhei Shchypanau <siarhei.shchypanau@gmail.com>
2022-06-29 02:38:17 +0300

    upd.

--- publish.sh
+++ publish.sh
@@ -6,5 +6,5 @@ VERSION=6.0.$JB_SPACE_EXECUTION_NUMBER
 
 echo "Publish nuget package"
 dotnet pack -o ./
-dotnet nuget push -s space
-dotnet nuget push -k 4d8b6735-05f8-3eeb-b7ed-023707ca17d9 -s https://repo.solidexpert.ltd/repository/nuget-hosted
+dotnet nuget push Solidex.Microservices.Core.6.0.1.nupkg -s space
+dotnet nuget push Solidex.Microservices.Core.6.0.1.nupkg -k 4d8b6735-05f8-3eeb-b7ed-023707ca17d9 -s https://repo.solidexpert.ltd/repository/nuget-hosted
